d68859adeeb29a4cfe4d6498c99806d0e2fbbde0
root <root@LAPTOP-T2LT97UU>
2023-11-01 14:44:33 -0300

    Second version

--- index.js
+++ index.js
@@ -1,93 +1,68 @@
-const express = require("express");
-const expressApp = require("express")();
-const axios = require("axios");
-
-const token = require("./modules/token");
-const register = require("./modules/registerUser");
+const app = require("express")();
 const port = 8080;
+const token = require("./lib/modules/token");
+const register = require("./lib/modules/registerUser");
+const cotacao = require("./lib/cotacao/api/cotacao");
+const info = require("./lib/info/api/info");
 
-expressApp.get("/register/:user/:createtoken", (req, res) => {
+app.get("/register/:user/:createtoken", (req, res) => {
   const user = req.params.user;
   const createToken = req.params.createtoken;
   if (user && createToken != null) {
     if (createToken == 1) {
       const userToken = token.generate_token();
       register.registerUser = { user: user, token: userToken };
-      res.send({
+      res.json({
         user: user,
         token: userToken,
       });
     } else {
-      res.send({ token: "Error falid params" });
+      res.json({ token: "Error falid params" });
     }
   }
 });
-
-expressApp.get("/login/:userLogin/:tokenLogin", (req, res) => {
+app.get("/login/:userLogin/:tokenLogin", (req, res) => {
   const userLogin = req.params.userLogin;
   const tokenLogin = req.params.tokenLogin;
   if (
     userLogin == register.registerUser.user &&
     tokenLogin == register.registerUser.token
   ) {
-    res.send(register.registerUser);
+    res.json(register.registerUser);
   } else {
     res.send({ error: "User or token incorrect" });
   }
 });
-
-expressApp.get("/login/:userLogin/:tokenLogin/cotacao/:moeda", (req, res) => {
+app.get("/login/:userLogin/:tokenLogin/cotacao/:moeda", (req, res) => {
   const userLogin = req.params.userLogin;
   const tokenLogin = req.params.tokenLogin;
   if (
     userLogin == register.registerUser.user &&
     tokenLogin == register.registerUser.token
   ) {
-    let moedacot = req.params.moeda;
-    let moedaConvercao = moedacot + "-BRL";
-    const url = `https://economia.awesomeapi.com.br/last/${moedaConvercao}`;
-    axios.get(url).then((e) => {
-      let dados = e.data;
-      res.json({ Cotacao: dados });
+    const moeda = req.params.moeda;
+    cotacao.cotGenerate(moeda).then((e) => {
+      res.json(e);
     });
   } else {
     res.send({ error: "User or token incorrect" });
   }
 });
-
-expressApp.get("/login/:userLogin/:tokenLogin/informacoes/:pais",(req, res) => {
-    let pais = req.params.pais;
-    const userLogin = req.params.userLogin;
-    const tokenLogin = req.params.tokenLogin;
-    if (
-      userLogin == register.registerUser.user &&
-      tokenLogin == register.registerUser.token
-    ) {
-      const url = `http://servicodados.ibge.gov.br/api/v1/paises/${pais}`;
-      axios.get(url).then((e) => {
-        const dados = e.data;
-        const dadosLocalidade = dados.map((e) => {
-          return {
-            nome: e.nome.abreviado,
-            lingua: e.linguas[0].nome,
-            historico: e.historico,
-            moeda: e?.["unidades-monetarias"]?.[0].id?.["ISO-4217-ALPHA"],
-          };
-        });
-        res.json({
-          nome: dadosLocalidade[0].nome,
-          Idioma: dadosLocalidade[0].lingua,
-          moeda: dadosLocalidade[0].moeda,
-          resumoHistorico: dadosLocalidade[0].historico,
-        });
-      });
-    } else {
-      res.send({ error: "User or token incorrect" });
-    }
-
+app.get("/login/:userLogin/:tokenLogin/informacoes/:pais", (req, res) => {
+  const userLogin = req.params.userLogin;
+  const tokenLogin = req.params.tokenLogin;
+  if (
+    userLogin == register.registerUser.user &&
+    tokenLogin == register.registerUser.token
+  ) {
+    const pais = req.params.pais;
+    info.infoGenerate(pais).then((e) => {
+      res.json(e);
+    });
+  } else {
+    res.json({ error: "User or token incorrect" });
   }
-);
-
-expressApp.listen(port, (e) => {
+});
+app.listen(port, (e) => {
   console.log("Open");
 });
